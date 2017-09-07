72203bd092d5546050995c11cb11f29f2e0414c7
Mohit Anand <anand.mohit001@gmail.com>
2017-09-07 06:15:34 +0530

    Model manager

--- urlshortener/models.py
+++ urlshortener/models.py
@@ -4,12 +4,33 @@ from django.db import models
 
 from .utils import code_genr,create_shortcode
 
+class KirrUrlManager(models.Manager):
+
+	def some_ran(self):
+		qs = KirrUrl.objects.all()
+		x = qs.count()
+
+		return x
+	def refresh_shortcodes(self):
+		qs = KirrUrl.objects.all()
+		new_codes = 0
+		for q in qs:
+			q.shortcode = create_shortcode(q)
+			q.save()
+			new_codes+=1
+		return "Number of new codes made {}".format(new_codes)
+
+
+
+
 class KirrUrl(models.Model):
 
 	url = models.CharField(max_length=300, )
 	shortcode = models.CharField(max_length=15, unique=True, blank=True)
 	timestamp_updated = models.DateTimeField(auto_now=True)
-	timestamp_created = models.DateTimeField(auto_now_add=True )
+	timestamp_created = models.DateTimeField(auto_now_add=True)
+
+	objects = KirrUrlManager()
 	
 	def __str__(self):
 		return str(self.url)
