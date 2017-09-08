d9ca866c20577ebdf34c76606b7e6ca80f695dbb
Mohit Anand <anand.mohit001@gmail.com>
2017-09-08 05:33:22 +0530

    Django forms for input

--- urlshortener/views.py
+++ urlshortener/views.py
@@ -2,12 +2,28 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
 from .models import KirrUrl
-# Create your views here.
-
+from .forms import SubmitUrlForm
 
 def home_view(request):
+	title = "Tirr.co"
+	if request.method == "POST":
+		the_form = SubmitUrlForm(request.POST)
+		if the_form.is_valid():
+			print (the_form.cleaned_data)
+		context = {
+			"form": the_form,
+			"title": title,
+		}
+		
+		return render(request, 'urlshortener/home.html', context)			
 
-	return render(request,'urlshortener/home.html',{})
+
+	the_form = SubmitUrlForm()
+	context = {
+		"form": the_form,
+		"title": title,
+	}
+	return render(request,'urlshortener/home.html',context)
 
 def kirr_redirect_view(request, shortcode,):
 
