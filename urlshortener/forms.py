7306fa31470e9c8d842e67c5963e4bf7ef53074f
Mohit Anand <anand.mohit001@gmail.com>
2017-09-08 07:48:30 +0530

    Improved overall looks

--- urlshortener/forms.py
+++ urlshortener/forms.py
@@ -3,4 +3,13 @@ from .validators import validate_url, validate_dot_com
 
 
 class SubmitUrlForm(forms.Form):
-	url = forms.CharField(label='Enter the url', validators=[validate_url, validate_dot_com])
+	url = forms.CharField(
+		label='',
+		validators=[validate_url, validate_dot_com],
+		widget = forms.TextInput(
+			attrs = {
+				"placeholder": "Long Url",
+				"class": "form-control"
+			}
+		)
+)
